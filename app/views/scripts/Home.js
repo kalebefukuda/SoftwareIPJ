bc75cce4168117d87604d76cc54506db07ff6c50
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2024-05-13 15:54:51 -0300

    rotas da pagina Home, com cliques funcionando

---
+++ app/views/scripts/Home.js
@@ -0,0 +1,14 @@
+// Rotas ao clicar
+
+document.getElementById('verMembrosBtn').addEventListener('click', function() {
+    window.location.href = '/membros'; // Redirecionar para a rota /membros
+});
+
+document.getElementById('sociedadesBtn').addEventListener('click', function(){
+    window.location.href = '/sociedades';
+})
+
+document.getElementById('relatoriosBtn').addEventListener('click', function(){
+    window.location.href = '/relatorios';
+})
+
