497d840523f46d46ba392f9c18983e57bae9f2d8
João Duarte <jpdx2010@hotmail.com>
2024-07-04 08:44:47 -0300

    Finalização da página de Membros

--- servidor.js
+++ servidor.js
@@ -16,7 +16,6 @@ const server = express();
 // habilitar o fileUpload
 server.use(fileUpload());
 
-
 // tratar requisições JSON
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
@@ -24,67 +23,64 @@ server.use(express.urlencoded({ extended: true }));
 // Configurar o Express.js para servir arquivos estáticos
 server.use(express.static(path.join(__dirname, 'src', 'views')));
 
-
-//Login
+// Login
 server.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'views', 'pages', 'Login.html'));
 });
 server.use("/login", loginRoutes);
-
-
 
 // Home
 server.get("/home", (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'views', 'pages', 'Home.html'));
 });
 
-//Cadastro 
+// Cadastro 
 server.get("/cadastro", cadastroRoutes);
 
-//Membro
-server.get("/membros", membroRoutes);
+// Página de Membros
+server.use("/membros", membroRoutes);
 
+// API de Membros
+server.use("/api", membroRoutes);
 
 // Relatorios
-server.get("/relatorios", relatorioRoutes)
-server.get("/relatorios/lista-aniversarios", relatorioRoutes)
-server.get("/relatorios/lista-comungantes-fem", relatorioRoutes)
-server.get("/relatorios/lista-comungantes-mas", relatorioRoutes)
-server.get("/relatorios/lista-nao-comungantes-fem", relatorioRoutes)
-server.get("/relatorios/lista-nao-comungantes-mas", relatorioRoutes)
-server.get("/relatorios/lista-comungantes-sede", relatorioRoutes)
-server.get("/relatorios/lista-data-casamento", relatorioRoutes)
-server.get("/relatorios/lista-chamada-assembleia", relatorioRoutes)
-server.get("/relatorios/lista-chamada-assembleia-adm", relatorioRoutes)
+server.get("/relatorios", relatorioRoutes);
+server.get("/relatorios/lista-aniversarios", relatorioRoutes);
+server.get("/relatorios/lista-comungantes-fem", relatorioRoutes);
+server.get("/relatorios/lista-comungantes-mas", relatorioRoutes);
+server.get("/relatorios/lista-nao-comungantes-fem", relatorioRoutes);
+server.get("/relatorios/lista-nao-comungantes-mas", relatorioRoutes);
+server.get("/relatorios/lista-comungantes-sede", relatorioRoutes);
+server.get("/relatorios/lista-data-casamento", relatorioRoutes);
+server.get("/relatorios/lista-chamada-assembleia", relatorioRoutes);
+server.get("/relatorios/lista-chamada-assembleia-adm", relatorioRoutes);
 
-server.get("/api/lista-aniversarios", relatorioRoutes)
-server.get("/api/lista-comungantes-fem", relatorioRoutes)
-server.get("/api/lista-comungantes-mas", relatorioRoutes)
-server.get("/api/lista-nao-comungantes-fem", relatorioRoutes)
-server.get("/api/lista-nao-comungantes-mas", relatorioRoutes)
-server.get("/api/lista-comungantes-sede", relatorioRoutes)
-server.get("/api/lista-data-casamento", relatorioRoutes)
-server.get("/api/lista-chamada-assembleia", relatorioRoutes)
-server.get("/api/lista-chamada-assembleia-adm", relatorioRoutes)
-
+server.get("/api/lista-aniversarios", relatorioRoutes);
+server.get("/api/lista-comungantes-fem", relatorioRoutes);
+server.get("/api/lista-comungantes-mas", relatorioRoutes);
+server.get("/api/lista-nao-comungantes-fem", relatorioRoutes);
+server.get("/api/lista-nao-comungantes-mas", relatorioRoutes);
+server.get("/api/lista-comungantes-sede", relatorioRoutes);
+server.get("/api/lista-data-casamento", relatorioRoutes);
+server.get("/api/lista-chamada-assembleia", relatorioRoutes);
+server.get("/api/lista-chamada-assembleia-adm", relatorioRoutes);
 
 // Cadastro
 server.get("/cadastro", (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'views', 'pages', 'CadastroMembro.html'));
 });
 
+// Sociedade Interna
+server.get("/sociedade-interna", sociedadeInternaRoutes);
+server.get("/sociedade-interna/cadastro", sociedadeInternaRoutes);
+server.get("/sociedade-cadastrada/:id", sociedadeInternaRoutes);
+server.get("/sociedade-interna/editar-sociedade/:id", sociedadeInternaRoutes);
 
-//Sociedade Interna
-server.get("/sociedade-interna", sociedadeInternaRoutes)
-server.get("/sociedade-interna/cadastro",sociedadeInternaRoutes)
-server.get("/sociedade-cadastrada/:id",sociedadeInternaRoutes)
-server.get("/sociedade-interna/editar-sociedade/:id",sociedadeInternaRoutes)
-
-server.get("/api/sociedade-cadastrada/:id",sociedadeInternaRoutes)
-server.get("/api/sociedade-interna",sociedadeInternaRoutes)
-server.post("/api/cadastro-sociedade",sociedadeInternaRoutes)
-server.put("/api/sociedade-interna/editar-sociedade/:id_sociedade_interna",sociedadeInternaRoutes)
-server.delete("/api/sociedade-interna/delete/:id_sociedade_interna",sociedadeInternaRoutes)
+server.get("/api/sociedade-cadastrada/:id", sociedadeInternaRoutes);
+server.get("/api/sociedade-interna", sociedadeInternaRoutes);
+server.post("/api/cadastro-sociedade", sociedadeInternaRoutes);
+server.put("/api/sociedade-interna/editar-sociedade/:id_sociedade_interna", sociedadeInternaRoutes);
+server.delete("/api/sociedade-interna/delete/:id_sociedade_interna", sociedadeInternaRoutes);
 
 // Configurando o servidor 
 const PORT = process.env.PORT || 3000;
--- src/views/scripts/Membro.js
+++ src/views/scripts/Membro.js
@@ -1,6 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function() {
+    try {
+        const response = await fetch('/membros/api/membros');
+        const membros = await response.json();
 
-    /* Função para aplicar classes de estilo do place-holder */
+        const membrosContainer = document.getElementById('membrosContainer');
+
+        membros.forEach(membro => {
+            const card = document.createElement('div');
+            card.classList.add('card-result');
+
+            card.innerHTML = `
+                <div class="img-result">
+                    <img class="campo-foto-card" src="../assets/Ellipse.png" alt="Foto do membro">
+                </div>
+                <div class="text-result-membro">
+                    <div class="text-icon">
+                        <h3 class="memberName">${membro.NOME}</h3>
+                        <button class="edit-icon">
+                            <ion-icon name="pencil-outline"></ion-icon>
+                        </button>
+                    </div>
+                    <div class="telefone-membro">
+                        <ion-icon name="phone-portrait-outline"></ion-icon>
+                        <h5>${membro.TELEFONE}</h5>
+                    </div>
+                </div>
+            `;
+
+            membrosContainer.appendChild(card);
+        });
+    } catch (error) {
+        console.error('Erro ao carregar membros:', error);
+    }
+
     function applyInitialSelectedClass(field) {
         if (field.value) {
             field.classList.remove('initial');
@@ -22,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
         applyInitialSelectedClass(campoSexo);
     }
 
-    //Comungante
+    // Comungante
     const campoComungante = document.getElementById('comungante');
     if (campoComungante) {
         campoComungante.addEventListener('change', function () {
@@ -32,7 +64,4 @@ document.addEventListener('DOMContentLoaded', function () {
         // Defina a classe inicial para o estado inicial
         applyInitialSelectedClass(campoComungante);
     }
-
-    /* ----------------------------------------------------------- */
-    
 });
